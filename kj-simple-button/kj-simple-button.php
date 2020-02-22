10406222c933d560331b217e59a47100821e5333
kjuraszek <donkrissocorleone@gmail.com>
2020-02-22 15:40:12 +0100

    Update kj-simple-button.php
    Options page

--- kj-simple-button/kj-simple-button.php
+++ kj-simple-button/kj-simple-button.php
@@ -17,7 +17,10 @@ class KJ_Simple_Floating_Button {
 	static $instance = false;
 
     public function __construct() {
-        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts') );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts') );
+		add_action( 'admin_menu', array($this, 'kj_simple_button_add_admin_menu') );
+		add_action( 'admin_init', array($this, 'kj_simple_button_settings_init') );
+		add_filter( 'plugin_action_links_'.plugin_basename(__FILE__), array($this, 'kj_simple_button_add_plugin_page_settings_link') );
 		add_action( 'wp_footer' , array($this, 'kj_simple_button_append_button') );
     }
 
@@ -31,9 +34,57 @@ class KJ_Simple_Floating_Button {
         wp_enqueue_style('KJ_Simple_Floating_Button', plugins_url('assets/css/style.css', __FILE__), null, '');
 	}
 	
+	public function kj_simple_button_add_admin_menu(  ) { 
+		add_options_page( 'KJ Simple Button', 'KJ Simple Button', 'manage_options', 'kj_simple_button', array($this, 'kj_simple_button_options_page') );
+	}
+	
+	public function kj_simple_button_add_plugin_page_settings_link( $plugin_links ) {
+		$plugin_links[] = '<a href="' .
+			admin_url( 'options-general.php?page=kj_simple_button' ) .
+			'">' . __('Settings') . '</a>';
+		return $plugin_links;
+	}
+
 	public function kj_simple_button_append_button(){
 		echo '<!-- #kj-simple-button -->';
 		echo '<a href="#" id="kj-simple-button">KJ</a>';
+	}
+
+	public function kj_simple_button_settings_init(  ) { 
+		
+		register_setting( 'kjSettingsPage', 'kj_simple_button_settings' );
+		add_settings_section(
+			'kj_simple_button_kjSettingsPage_section_style', 
+			__( 'Style settings', 'kj-simple-button' ), 
+			array($this, 'kj_simple_button_settings_section_callback'), 
+			'kjSettingsPage'
+		);
+	}
+
+	public function kj_simple_button_settings_section_callback(  ) { 
+		// callback
+		echo __( '', 'kj-simple-button' );
+
+	}
+
+	public function kj_simple_button_options_page() {
+		
+		?>
+		<form action='options.php' method='post'>
+
+			<h2>KJ Simple Button</h2>
+
+			<?php
+
+			settings_fields( 'kjSettingsPage' );
+			do_settings_sections( 'kjSettingsPage' );
+			submit_button();
+			
+			?>
+
+		</form>
+		<?php
+
 	}
 
 }
